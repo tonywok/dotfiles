0a9d028804de1b824d97bbfe0a5224e8453e6e7d
Tony Schneider <tonywok@gmail.com>
2014-06-22 00:39:37 -0400

    Links - of the symbolic variety

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -sf ./src/.vimrc ~/.vimrc
+ln -sf ./src/.vim ~/.vim
+
+ln -sf ./src/.bashrc ~/.bashrc
+ln -sf ./src/.bash_profile ~/.bash_profile
+ln -sf ./src/.bash ~/.bash
+
+ln -sf ./src/.gitconfig ~/.gitconfig
