8f8fca5dcb4e2b8b1870aaa622c4b1e5f0ce8d0f
Tony Schneider <tonywok@gmail.com>
2014-06-23 23:36:48 -0400

    Remove mvim alias bc it is now installed via homebrew

--- src/.bash/aliases.sh
+++ src/.bash/aliases.sh
@@ -1,4 +1,2 @@
-alias gvim='/Applications/MacVim.app/Contents/MacOS/Vim -g'
-alias mvim='/Applications/MacVim.app/Contents/MacOS/Vim -g'
 alias resolve='mvim -p $(git diff --name-only --diff-filter=U | xargs)'
 alias wee=weechat-curses
