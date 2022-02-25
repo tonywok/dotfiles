c3e2f41d795a125f0d0ead3e87e39f522743c31b
Tony Schneider <tonywok@gmail.com>
2022-02-25 12:06:24 +0000

    bits and bobs

--- src/.bash/aliases.sh
+++ src/.bash/aliases.sh
@@ -1,5 +1,6 @@
 alias resolve='mvim -p $(git diff --name-only --diff-filter=U | xargs)'
 alias gst='git status'
+alias grmb='git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d'
 __git_complete gst _git_status
 alias gco="git checkout"
 __git_complete gco _git_checkout
--- src/.bashrc
+++ src/.bashrc
@@ -1,6 +1,5 @@
 export EDITOR="vim"
 export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
 export HISTSIZE=""
 
 source $(brew --prefix)/etc/bash_completion
